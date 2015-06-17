d6d07915265afa483cdee42a032ef405bd15fb97
Andri Oskarsson <andri80@gmail.com>
2015-06-17 16:07:19 +0200

    first steps with cli, out of time... finish later

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/BurntSushi/toml"
+	"github.com/codegangsta/cli"
 	"github.com/gorilla/mux"
 	"net/http"
-	//"os"
-	"github.com/BurntSushi/toml"
+	"os"
 )
 
 type PreviewOptions struct {
@@ -31,17 +32,75 @@ func init() {
 }
 
 func main() {
-	if _, err := toml.DecodeFile("config.toml", &configuration); err != nil {
-		// handle error
-		panic(err)
+
+	app := cli.NewApp()
+	app.Name = "s3preview"
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "Andri Óskarsson",
+			Email: "andri80@gmail.com",
+		},
+	}
+	app.Usage = "Previews (read thumbnails) for AWS S3"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "s3-key",
+			Usage:  "AWS Access Key. Needs to have S3 Access.",
+			EnvVar: "AWS_ACCESS_KEY_ID",
+		},
+		cli.StringFlag{
+			Name:   "s3-secret-key",
+			Usage:  "AWS Secret Key",
+			EnvVar: "AWS_SECRET_ACCESS_KEY",
+		},
+		cli.IntFlag{
+			Name:   "listen-http, l",
+			Usage:  "HTTP listen port",
+			Value:  80,
+			EnvVar: "HTTP_PORT",
+		},
+		cli.StringFlag{
+			Name:   "asset-bucket",
+			Usage:  "Bucket to retrieve originals from",
+			EnvVar: "ASSET_BUCKET",
+		},
+		cli.StringFlag{
+			Name:   "asset-prefix",
+			Usage:  "Prefix for Asset Bucket. E.g. /originals",
+			EnvVar: "ASSET_PREFIX",
+		},
+		cli.StringFlag{
+			Name:   "preview-bucket",
+			Usage:  "Bucket to store previews on. Needs to be public for redirects to work.",
+			EnvVar: "PREVIEW_BUCKET",
+		},
+		cli.StringFlag{
+			Name:   "preview-prefix",
+			Usage:  "Prefix for Preview Bucket. E.g. /s3preview",
+			Value:  "/s3preview",
+			EnvVar: "ASSET_PREFIX",
+		},
 	}
 
-	r := mux.NewRouter()
+	// TODO: Finish CLI'fying the program
 
-	http.Handle("/", r)
-	registerHandlers(r)
-	//fmt.Println(configuration.Previews)
-	//	fmt.Println(configuration.Previews["small"])
-	fmt.Printf("I'm listening (port %d)...\n", configuration.ListenPort)
-	http.ListenAndServe(fmt.Sprintf(":%d", configuration.ListenPort), nil)
+	app.Action = func(c *cli.Context) {
+		if _, err := toml.DecodeFile("config.toml", &configuration); err != nil {
+			// handle error
+			panic(err)
+		}
+
+		r := mux.NewRouter()
+
+		http.Handle("/", r)
+		registerHandlers(r)
+		//fmt.Println(configuration.Previews)
+		//	fmt.Println(configuration.Previews["small"])
+		fmt.Printf("I'm listening (port %d)...\n", configuration.ListenPort)
+		http.ListenAndServe(fmt.Sprintf(":%d", configuration.ListenPort), nil)
+	}
+
+	app.Run(os.Args)
+
 }
